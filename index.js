e3bf1350c29ae962cee312b2ae4db1cded6cc0db
Facundo Rubin <facundorubin@gmail.com>
2016-01-27 17:30:06 -0300

    Cambie url por una de subtitulos traducidos

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var request = require('request'),
 	cheerio	= require('cheerio');
 
-var url='http://www.tusubtitulo.com/';
+var url='http://www.tusubtitulo.com/ajax_tabs.php?mode=translated&page=1&max=10';
 
 request(url, function(error, response, html)
 {
@@ -14,7 +14,11 @@ request(url, function(error, response, html)
 		$('.line1').filter(function()
 			{
 				var dato= $(this);
-				console.log(dato.children().text());
+
+				titulos = dato.children().text();
+
+				//release = data.children().last().children().text();
+				console.log(titulos);
 			});
 	}
 });	
